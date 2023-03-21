0a223946d1f8486a9d7d0a5ba332b7f511190533
dreamking60 <dreamlyboyczh@foxmail.com>
2023-03-21 21:21:53 +0800

    Macro Write! faile

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::str::Bytes;
-use core::{arch::global_asm};
-use core::fmt;
-use core::fmt::Write;
+use core::{arch::global_asm, fmt::Write};
 
 mod panic;
 
@@ -14,21 +11,22 @@ include!("uart_console.rs");
 #[no_mangle]
 pub extern "C" fn hypervisor_main() {
     print_something();
-}   
+}
 
 pub fn print_something() {
-    let mut writer = Writer{};
+    let mut writer = Writer {};
 
-    // 测试Writer我们实现的两个函数
+    // Test the two functions we implemented for Writer
     writer.write_byte(b'H');
     writer.write_string("ello ");
     writer.write_string("Wörld!\n");
-    
+
     writer.write_string("[0] Hello from Rust!\n");
 
-    // 验证实现core::fmt::Write自动实现的方法
-    let display: fmt::Arguments = format_args!("hello arguments!\n");
+    // Verify the auto-implemented methods from core::fmt::Write
+    let display = format_args!("hello arguments!\n");
     writer.write_fmt(display).unwrap();
-    // 使用write!宏进行格式化输出
-    write!(writer, "The numbers are {} and {} \n", 42, 1.0/3.0).unwrap();
+
+    // Use the write! macro for formatted output
+    write!(writer, "The numbers are {} and {} \n", 42, 1.0 / 3.0).unwrap();
 }
